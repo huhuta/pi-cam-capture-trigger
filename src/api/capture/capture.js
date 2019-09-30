9b3884a338ec27451b5aa285e16dde7a5ed510e8
yoyota <smarthorse6@gmail.com>
2019-10-01 08:45:25 +0900

    Add camera 3

--- src/api/capture/capture.js
+++ src/api/capture/capture.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 exports.capture = async (req, res) => {
   const { section, label } = req.params
-  const promises = [...Array(3)].map((_, i) =>
+  const promises = [...Array(4)].map((_, i) =>
     axios.get(
       `https://pi-cam-${i}.dudaji.org/${section}${label ? `/${label}` : ''}`
     )
